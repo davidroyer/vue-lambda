27c3ab1bdccf3e822ea035a91f37bb4fef2c58e7
David Royer <droyer01@gmail.com>
2018-03-27 17:11:14 -0400

    added api from github

---
+++ src/lambda/api.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch').default;
+
+export function handler(event, context, callback) {
+  // const request = await fetch('https://api.github.com/orgs/zeit/members')
+  // var json;
+  fetch('https://api.github.com/orgs/zeit/members')
+  	.then(res => res.json())
+  	.then(json => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(json)
+      })
+    });
+
+}
